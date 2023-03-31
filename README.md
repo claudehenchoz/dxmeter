30fea3f847c1b94f6c4087b6117b3f1722706f31
Claude Henchoz <claude.henchoz@gmail.com>
2023-03-31 12:17:09 +0200

    Updated to run first build

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 > A framework for speed-testing a standardized set of developer activities across platforms
 
+Use this for measuring impact: https://github.com/google/UIforETW/releases/download/v1.58/etwpackage1.58.zip
--- meter-dubbo.ps1
+++ meter-dubbo.ps1
@@ -1,39 +1,37 @@
 $rootFolder = "C:\CHDEV"
 
-# Download prereqs
-$urlsToFolders = @{
-    "https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.zip" = "$rootFolder\apache-maven-3.9.1"
-    "https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_windows-x64_bin.zip" = "$rootFolder\openjdk-19.0.2"
-    "https://github.com/apache/dubbo/archive/refs/tags/dubbo-3.1.8.zip" = "$rootFolder\dubbo-3.1.8"
+# Define target folders and corresponding URLs
+$foldersToUrls = @{
+    "apache-maven-3.9.1" = "https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.zip"
+    "jdk-19.0.2" = "https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_windows-x64_bin.zip"
+    "dubbo-dubbo-3.1.8" = "https://github.com/apache/dubbo/archive/refs/tags/dubbo-3.1.8.zip"
 }
 
-foreach ($url in $urlsToFolders.Keys) {
-    $targetFolder = $urlsToFolders[$url]
+# Download and extract prereqs
+foreach ($folder in $foldersToUrls.Keys) {
+    $url = $foldersToUrls[$folder]
+    $targetFolder = Join-Path $rootFolder $folder
 
     if (-not (Test-Path $targetFolder)) {
         New-Item -ItemType Directory -Path $targetFolder | Out-Null
-
-        $zipFilePath = Join-Path $targetFolder "download.zip"
+        $zipFilePath = Join-Path $rootFolder "download.zip"
         $ProgressPreference = 'SilentlyContinue'
         Invoke-WebRequest -Uri $url -OutFile $zipFilePath
+        Expand-Archive -Path $zipFilePath -DestinationPath $rootFolder -Force
         $ProgressPreference = 'Continue'
-
-        Expand-Archive -Path $zipFilePath -DestinationPath $targetFolder -Force
         Remove-Item $zipFilePath
     }
 }
 
+# Set environment variables
+$env:MAVEN_HOME = Join-Path $rootFolder "apache-maven-3.9.1"
+$env:Path += ";$($env:MAVEN_HOME)\bin"
 
-# Prepare env
-$env:MAVEN_HOME = "$rootFolder\apache-maven-3.9.1\apache-maven-3.9.1"
-$env:Path += ";$rootFolder\apache-maven-3.9.1\apache-maven-3.9.1\bin"
+$env:JAVA_HOME = Join-Path $rootFolder "jdk-19.0.2"
+$env:Path += ";$($env:JAVA_HOME)\bin"
 
-$env:JAVA_HOME = "$rootFolder\openjdk-19.0.2\jdk-19.0.2"
-$env:Path += ";$rootFolder\openjdk-19.0.2\jdk-19.0.2\bin"
-
-Push-Location "$rootFolder\dubbo-3.1.8\dubbo-dubbo-3.1.8"
-
+# Build and install using Maven
+$projectFolder = Join-Path $rootFolder "dubbo-dubbo-3.1.8"
+Push-Location $projectFolder
 mvn clean install -DskipTests
-
-
 Pop-Location
